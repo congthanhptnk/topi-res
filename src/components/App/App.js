33598194c4ccfb1d5d4bbb55e4d99285b4ad3d0c
Thanh Tran <congthanhptnk@gmail.com>
2019-08-21 11:18:09 +0300

    basic Header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from '../reusable/Button';
+import Header from '../reusable/Header';
 import styles from './App.module.css';
 
 function App() {
   return (
     <main className={styles.layout}>
-      <p>Something</p>
+      <Header title="Header"/>
       <Button type="submit" value="Add" text="OK" />
     </main>
   );
