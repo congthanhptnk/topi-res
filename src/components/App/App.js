2cdbcdf4f709baba64feec8ccaca17b84b5de3e5
Thanh Tran <congthanhptnk@gmail.com>
2019-08-21 10:20:17 +0300

    Button component UI config

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from '../reusable/Button';
 
 function App() {
   return (
     <div>
       <p>Something</p>
+      <Button type="submit" value="Add" text="OK" />
     </div>
   );
 }
---
+++ src/components/reusable/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+const Button = (props) => {
+  return (
+    <button type={props.type} value={props.value} className={styles.button} >
+      {props.text}
+    </button>
+  )
+};
+
+export default Button;
