770aa3178aed4d7cc6485145487b3172cd0d37bc
Thanh Tran <congthanhptnk@gmail.com>
2019-08-21 10:33:02 +0300

    Layout and button basic styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from '../reusable/Button';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div>
+    <main className={styles.layout}>
       <p>Something</p>
       <Button type="submit" value="Add" text="OK" />
-    </div>
+    </main>
   );
 }
 
