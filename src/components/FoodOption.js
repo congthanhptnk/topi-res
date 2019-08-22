7a623b6e7de8676c239a901ec6947e62bd24c936
Thanh Tran <congthanhptnk@gmail.com>
2019-08-22 09:49:56 +0300

    Properly display Menu items using state hooks

--- src/components/FoodOption.js
+++ src/components/FoodOption.js
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from './FoodOption.module.css';
 import Button from './reusable/Button';
 
-const FoodOption = ({ name, ingredients }) => {
+const FoodOption = ({ name, ingredients, price }) => {
   return(
     <div className={styles.container}>
       <div className={styles.textContainer}>
         <p className={styles.name}>{name}</p>
         <p className={styles.ingredients}>{ingredients}</p>
       </div>
-      <p className={styles.price}>50</p>
+      <p className={styles.price}>{price}</p>
       <Button type="submit" text="Select"/>
     </div>
   );
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FoodOption from './FoodOption';
+import FoodItem from '../models/FoodItem';
 import styles from './Menu.module.css';
 
 const Menu = () => {
+  const [ state, setState ] = useState(MenuList);
+  const { menu } = state;
+
+  const renderMenu = () => {
+    return menu.map(foodItem => {
+      return(
+        <FoodOption
+          key={foodItem.id}
+          name={foodItem.name}
+          ingredients={foodItem.ingredients}
+          price={foodItem.price}
+        />
+      );
+    });
+  };
+
   return(
     <div className={styles.menu}>
-      <FoodOption name="Sushi" ingredients="Salmon, rice, vinegar" />
-      <FoodOption name="Sushi" ingredients="Salmon, rice, vinegar" />
+      {renderMenu()}
     </div>
   )
+};
+
+const MenuList = {
+  menu: [
+    new FoodItem("Sushi", 50, "Salmon, rice, vinegar", 1 ),
+    new FoodItem("Burger", 20, "Beef, bread, lettuce", 2 ),
+    new FoodItem("Water", 10, "None", 3 ),
+    new FoodItem("Pho", 20, "Beef, rice noodle, onion", 4 ),
+    new FoodItem("Fried egg", 570, "Egg, vege oil", 5 ),
+    new FoodItem("Tea", 90, "Tea bag, water, sugar", 6 ),
+    new FoodItem("Pizza", 70, "Pork, pineapple, bread", 7 )
+  ]
 };
 
 export default Menu;
